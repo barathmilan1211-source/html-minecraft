4cd5f62887f2ae7ed9314210513896060438b378
barathmilan1211-source <barathmilan1211@gmail.com>
2026-05-02 22:02:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+/* Az óra frissítése UTC formátumban[cite: 1] */
 function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+    const timeDisplay = document.getElementById('currentTime');
+    if (timeDisplay) {
+        timeDisplay.innerHTML = new Date().toUTCString();[cite: 1]
+    }
 }
+
+// Indítás és ismétlés 1 másodpercenként[cite: 1]
 showTime();
 setInterval(function () {
-	showTime();
-}, 1000);
+    showTime();
+}, 1000);[cite: 1]
